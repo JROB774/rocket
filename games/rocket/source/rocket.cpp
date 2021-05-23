5f7aa00ba741b6c0d45965dbea4d0c6b6d619327
Joshua Robertson <joshrobertson774@gmail.com>
2021-05-23 20:05:33 +0100

    Lowered the number of smoke particles spawned by Rainbow Rocket.

--- games/rocket/source/rocket.cpp
+++ games/rocket/source/rocket.cpp
@@ -651,7 +651,7 @@ static void UpdateRocket(f32 dt)
             SmokeType smokeType = (s_rocket.costume == Costume_Meat) ? SmokeType_Blood : SmokeType_Thruster;
             s32 smokeCount = 1;
             if(s_rocket.costume == Costume_Meat) smokeCount = 2;
-            if(s_rocket.costume == Costume_Rainbow) smokeCount = 4;
+            if(s_rocket.costume == Costume_Rainbow) smokeCount = 2;
             SpawnSmoke(smokeType, s_rocket.pos.x+RandomF32(-3.0f,3.0f), s_rocket.pos.y+20.0f, smokeCount);
             s_rocket.timer -= 0.05f;
         }
