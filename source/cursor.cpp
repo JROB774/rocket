c413a59fe9900438b359f18dbf95e6fcc19c41d3
Joshua Robertson <joshrobertson774@gmail.com>
2022-10-03 21:33:20 +0100

    Improve unfocused mode + get it working properly on Windows.

--- source/cursor.cpp
+++ source/cursor.cpp
@@ -3,7 +3,8 @@ static bool s_wantLockCursor = true;
 #ifndef __EMSCRIPTEN__
 bool CanvasHasFocus()
 {
-    return true;
+    u32 windowFlags = SDL_GetWindowFlags(s_context.window);
+    return CHECK_FLAGS(windowFlags, SDL_WINDOW_INPUT_FOCUS);
 }
 #else
 EM_JS(bool, CanvasHasFocus, (),
@@ -16,14 +17,17 @@ EM_JS(bool, CanvasHasFocus, (),
 static void UpdateCursor(f32 dt)
 {
     s_wantLockCursor = (!s_gamePaused && !s_rocket.dead && (s_gameState == GameState_Game));
-    s_gameUnfocused = !CanvasHasFocus();
+    s_gameUnfocused = ((s_gameState == GameState_Game) && !CanvasHasFocus());
 
     #ifndef __EMSCRIPTEN__
     LockMouse(s_wantLockCursor);
-    #else
-    if(!CanvasHasFocus() && s_wantLockCursor)
-        PauseGame();
     #endif // __EMSCRIPTEN__
+
+    // Pause the game if we've lost focus.
+    if(s_gameUnfocused && !s_gamePaused)
+    {
+        PauseGame();
+    }
 }
 
 static void RenderCursor(f32 dt)
