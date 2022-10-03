c7b1c3dc0be5fc804fcbf290bcfe5563a380b843
Joshua Robertson <joshrobertson774@gmail.com>
2022-10-03 22:57:10 +0100

    Add the costume unlock notifications.

--- source/menu_gameover.cpp
+++ source/menu_gameover.cpp
@@ -1,4 +1,9 @@
 static bool s_hasDoneInitialGameOverUnlockClick;
+static f32  s_unlockCostumeScale;
+static f32  s_unlockCostumeAngle;
+static f32  s_unlockRadialAngle;
+static f32  s_unlockRadialAlpha;
+static f32  s_unlockRadialScale;
 
 static void GameOverMenuActionRetry(MenuOption& option)
 {
@@ -16,6 +21,14 @@ MenuOption(GameOverMenuActionRetry, MenuOptionType_Button, { 0.0f,248.0f,180.0f,
 MenuOption(GameOverMenuActionMenu,  MenuOptionType_Button, { 0.0f,272.0f,180.0f,24.0f }, { 0, 336,128,24 })
 };
 
+static void SetupNextUnlockState()
+{
+    PlaySound("unlock");
+    s_unlockCostumeScale = 0.0f;
+    s_unlockRadialAlpha = 0.0f;
+    s_unlockRadialScale = 0.0f;
+}
+
 static void UpdateGameOverMenu(f32 dt)
 {
     if(s_gameState != GameState_Game) return;
@@ -25,15 +38,17 @@ static void UpdateGameOverMenu(f32 dt)
     if(!s_gameOverUnlocks.empty())
     {
         if(!s_hasDoneInitialGameOverUnlockClick)
+        {
             s_hasDoneInitialGameOverUnlockClick = IsMouseButtonPressed(MouseButton_Left);
+            if(s_hasDoneInitialGameOverUnlockClick) SetupNextUnlockState();
+        }
         else
         {
-            // @Incomplete: ...
-
             // Move on to the next unlock, or the game over menu.
             if(IsMouseButtonPressed(MouseButton_Left))
             {
                 s_gameOverUnlocks.erase(s_gameOverUnlocks.begin());
+                if(!s_gameOverUnlocks.empty()) SetupNextUnlockState();
             }
         }
     }
@@ -50,10 +65,41 @@ static void RenderGameOverMenu(f32 dt)
     if(s_gameState != GameState_Game) return;
     if(s_gameResetting || !s_rocket.dead) return;
 
+    static f32 s_timer = 0.0f;
+
+    s_timer += dt;
+
+    s_unlockCostumeAngle = SinRange(-10.0f, 10.0f, s_timer*2.5f);
+    s_unlockRadialAngle += (dt * 2.0f);
+
     // If we have unlocked stuff display that first before the actual game over menu.
     if(!s_gameOverUnlocks.empty())
     {
-        // @Incomplete: ...
+        if(s_hasDoneInitialGameOverUnlockClick)
+        {
+            s_unlockCostumeScale = nk::lerp(s_unlockCostumeScale, 2.0f, dt * 12.0f);
+            s_unlockRadialScale  = nk::lerp(s_unlockRadialAlpha,  1.0f, dt *  9.0f);
+            s_unlockRadialAlpha  = nk::lerp(s_unlockRadialAlpha,  1.0f, dt *  5.0f);
+
+            Costume costume = s_gameOverUnlocks[0];
+
+            f32 halfW = GetScreenWidth() * 0.5f;
+            f32 halfH = GetScreenHeight() * 0.5f;
+
+            f32 costumeOffset = 64 * CAST(f32, costume);
+            f32 nameOffset = 24 * CAST(f32, costume);
+
+            Rect costumeClip = { 64+costumeOffset,0,64,64 };
+            Rect nameClip = { 0,624+nameOffset,256,24 };
+            Rect unlockClip = { 0,1784,256,24 };
+
+            nkVec4 color = { 1,1,1,s_unlockRadialAlpha };
+
+            imm::DrawTexture("radial", halfW,halfH, s_unlockRadialScale,s_unlockRadialScale, s_unlockRadialAngle, imm::Flip_None, NULL, NULL, color);
+            imm::DrawTexture("costume", halfW,halfH, s_unlockCostumeScale,s_unlockCostumeScale, nk::torad(s_unlockCostumeAngle), imm::Flip_None, NULL, &costumeClip);
+            imm::DrawTexture("menu", halfW,halfH + 80.0f, &nameClip);
+            imm::DrawTexture("menu", halfW,halfH + 96.0f, &unlockClip);
+        }
     }
     else
     {
